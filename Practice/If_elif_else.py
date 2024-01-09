8a1c94ef47f429b76ebf406e686e428fbca45188
Debashis7307 <debashisalak@gmail.com>
2024-01-09 23:40:51 +0530

    hii

--- Practice/If_elif_else.py
+++ Practice/If_elif_else.py
@@ -2,7 +2,7 @@ age = int(input("Tell me your age ?"))
 if age == 100:
     print("100 sal ka budha hay to!")
 elif age >= 18 and age <=100:
-    print("You are adult!")
+    print("You are just adult!")
 elif age <0:
     print("You have not born yeat!")
 elif age >= 0 and age <=17:
